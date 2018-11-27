8a96202ba26b6e42fa86dcc6fd5c6c08a574e843
babatonha <40696889+babatonha@users.noreply.github.com>
2018-11-27 13:30:16 +0200

    Add files via upload

---
+++ graduateAttire/court.php
@@ -0,0 +1,83 @@
+<h1 style="text-align: center;">Your Court attire and prices are coming soon!!!!</h1>
+
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>African attire</title>
+ 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+ </head>
+ <body>
+
+
+
+
+
+ 	<div class="row">
+    <div class="col-md-12">
+
+        <div id="mdb-lightbox-ui"></div>
+
+        <div class="mdb-lightbox no-margin">
+
+            <figure class="col-md-4">
+                <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg" data-size="1600x1067">
+                    <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(117).jpg" class="img-fluid">
+                </a>
+            </figure>
+
+            <figure class="col-md-4">
+                <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(98).jpg" data-size="1600x1067">
+                    <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(98).jpg" class="img-fluid" />
+                </a>
+            </figure>
+
+            <figure class="col-md-4">
+                <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(131).jpg" data-size="1600x1067">
+                    <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(131).jpg" class="img-fluid" />
+                </a>
+            </figure>
+
+            <figure class="col-md-4">
+                <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(123).jpg" data-size="1600x1067">
+                    <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(123).jpg" class="img-fluid" />
+                </a>
+            </figure>
+
+            <figure class="col-md-4">
+                <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(118).jpg" data-size="1600x1067">
+                    <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(118).jpg" class="img-fluid" />
+                </a>
+            </figure>
+
+            <figure class="col-md-4">
+                <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(128).jpg" data-size="1600x1067">
+                    <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(128).jpg" class="img-fluid" />
+                </a>
+            </figure>
+
+            <figure class="col-md-4">
+                <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(132).jpg" data-size="1600x1067">
+                    <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(132).jpg" class="img-fluid" />
+                </a>
+            </figure>
+
+            <figure class="col-md-4">
+                <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(115).jpg" data-size="1600x1067">
+                    <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(115).jpg" class="img-fluid" />
+                </a>
+            </figure>
+
+            <figure class="col-md-4">
+                <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(133).jpg" data-size="1600x1067">
+                    <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(133).jpg" class="img-fluid" />
+                </a>
+            </figure>
+
+        </div>
+
+    </div>
+</div>
+ 
+ </body>
+ </html>
---
+++ graduateAttire/index.php
@@ -0,0 +1,185 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Graduation Attire</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="main.css">
+  <link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet">
+
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+   <link href='https://fonts.googleapis.com/css?family=Anton' rel='stylesheet' type='text/css'>
+   <link href='https://fonts.googleapis.com/css?family=Neucha' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+<!--Navigation bar-->
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+    </div>
+
+    <div class="collapse navbar-collapse" id="myNavbar">
+      
+         <div class="navbar">
+          <div class="logo_div">
+            <a href="index.php"><h1>GraduateAttire</h1></a>
+          </div>
+          <ul class="nav navbar-nav navbar-right">
+          <ul>
+            <li><a class="active" href="index.php">Home</a></li>
+            <li><a href="graduate.php">Graduation</a></li>
+            <li><a href="court.php">Court Attire</a></li>
+            <li><a href="african.php">African Attire</a></li>
+            <li><a href="index.php#about">About</a></li>
+            <li><a href="contact-form/formpage.html">Contact</a></li>
+            
+          </ul>
+        </div>   
+       </ul>
+      </ul>
+    </div>
+  </div>
+</nav>
+ <!--End of Navigation Bar-->
+
+<div class="banner">
+  <h1> <br><br>The graduate Attire Hub</h1>
+  <p>Look glamour at your big day</p>  
+</div> 
+
+<div class="body">
+<!--Projects-->
+<div class="projects">
+<h1 class="projects-heading">What we can Do</h1>
+
+  <div class="row">
+      <div class="col-sm-4" >
+         <h4>African attire</h4>
+         <a href="african.php"><img  src="images/afri" style="height: 220px; width: 300px;">
+          <p><b>We can design the best African attire</b><br><i>Click here to see more...</i></p>
+          </a>
+      </div>
+          <div class="col-sm-4">
+            <h4>Graduation attire</h4>
+             <a href="graduate.php"><img style="height: 220px; width: 300px;" src="images/gown">
+              <p><b>We can design the best Graduation attire</b><br><i>Click here to see more...</i></p>
+              </a>
+          </div>
+      <div class="col-sm-4">
+        <h4>Court attire</h4>
+          <a href="court.php"><img style="height: 220px; width: 300px;" src="images/legg">
+          <p><b>We can design the best court attire</b><br><i>Click here to see more...</i></p>
+          </a>
+      </div>
+  </div>
+</div>
+
+
+<!--ABOUT-->
+<div class="about">
+<h1 id="about"> About</h1>
+<p>Our compnay is directed By Tsitsi Mubuyi. We are the best in town We are here to dress you on your big days</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+<h4>Meet our Staff</h4>
+
+<div class="row">
+    <div class="col-sm-3" >
+          <img  src="images/profile" style="height: 220px; width: 250px;">
+          <p><h5><b>Tsitsi Mubuyi</b></h5>Director</p>  
+    </div>
+
+    <div class="col-sm-3" >
+          <img  src="images/myself1.png" style="height: 220px; width: 250px;">
+          <p><h5><b>Tonha View</b></h5>I.T specialist</p>  
+    </div>
+
+    <div class="col-sm-3" >
+          <img  src="images/profile" style="height: 220px; width: 250px;">
+          <p><h5><b>xxxxxxx</b></h5>xxxxxx</p>  
+    </div>
+
+    <div class="col-sm-3" >
+          <img  src="images/profile" style="height: 220px; width: 250px;">
+          <p><h5><b>xxxxx</b></h5>xxxxx</p>  
+    </div>
+
+    
+  </div>
+
+</div>
+
+  <h1>Physical Address</h1>
+    <div class="row">
+      <div class="col-sm-6" >
+        <p>Cnr Bossman & Francis Bard Street <br>Ultra Building<br> Pretoria<br><br><strong>Email:</strong> tsitsimachiri@gmail.com<br>OR: graduatesattire@yahoo.com<br> <br><strong>Cell:</strong> 060 494 4440<br>OR: 081 530 4619</p>
+        <p>We are there to offer you the best service you can trust<br><h3>Our Opening Times</h3> Monday - Friday  8:30AM - 4:00PM<br>Saturdays: 8:30AM - 12PM<br>Sundays: Closed</p>
+      </div>
+
+      <div class="col-sm-6" >
+        <div>
+           <iframe width="500" height="400" frameborder="0" src="https://www.bing.com/maps/embed?h=400&w=500&cp=-25.749192000000008~28.185783999999994&lvl=16&typ=d&sty=r&src=SHELL&FORM=MBEDV8" scrolling="no">
+           </iframe>
+           <div style="white-space: nowrap; text-align: center; width: 500px; padding: 6px 0;">
+              <a id="largeMapLink" href="https://www.bing.com/maps?cp=-25.749192000000008~28.185783999999994&amp;sty=r&amp;lvl=16&amp;FORM=MBEDLD" target="_blank">View Larger Map</a> &nbsp; | &nbsp;
+              <a id="dirMapLink" href="https://www.bing.com/maps/directions?cp=-25.749192000000008~28.185783999999994&amp;sty=r&amp;lvl=16&amp;rtp=~pos.-25.749192000000008_28.185783999999994____&amp;FORM=MBEDLD" target="_blank">Get Directions</a>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+ 
+
+</div><!--end of body-->
+
+
+<!--FOOTER-->
+ <footer class="footer-distributed">
+
+      <div class="footer-right">
+
+        <a href="https://facebook.com/"><i class="fa fa-facebook"></i></a>
+        <a href="https://twitter.com/"><i class="fa fa-twitter"></i></a>
+        <a href="#"><i class="fa fa-linkedin"></i></a>
+        <a href="#"><i class="fa fa-github"></i></a>
+
+      </div>
+
+      <div class="footer-left">
+
+        <p class="footer-links">
+          <a class="link-1" href="landing.php">Home</a>
+
+          <a href="landing.php">Blog</a>
+
+          <a href="fee.php">Pricing</a>
+
+          <a href="#">About</a>
+
+          <a href="#">Faq</a>
+
+          <a href="#">Contact</a>
+        </p>
+
+        <p>SendUs by Tonha View &copy; 2018</p>
+      </div>
+
+    </footer>
+
+
+
+
+
+</body>
+</html>
+
